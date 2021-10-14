3e90164ef655dc8d80d5caf24de9766cf9a8cfe1
xhandram <belladamix@gmail.com>
2021-10-14 15:58:38 +0200

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,10 +1,8 @@
-window.addEventListener('load', function(){
-    new Glider(document.querySelector('.glider'), {
-        slidesToShow: 1,
-        dots: '#dots',
-        arrows: {
-          prev: '#prev-slide',
-          next: '#next-slide'
-        }
-    })
-  })
+//FOR THE ACORDEON
+const centros = document.querySelectorAll(".contact__centro");
+
+centros.forEach((centro) => {
+  centro.addEventListener("click", () => {
+    centro.classList.toggle("active");
+  });
+});
